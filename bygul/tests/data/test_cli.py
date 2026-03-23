88e8f60941f6e86175cd7c4ae7cebc445b0fe2e8
Maryam <mary.elf97@gmail.com>
2026-03-23 13:38:31 -0700

    editing test functions to ensure they match the new changes

--- bygul/tests/data/test_cli.py
+++ bygul/tests/data/test_cli.py
@@ -15,8 +15,8 @@ class CommandLineTests(unittest.TestCase):
         os.system(
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed \
-            bygul/tests/data/reference.fasta --redo"
+            --primers bygul/tests/data/ARTIC_V4-1.bed \
+            --reference bygul/tests/data/reference.fasta --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
 
@@ -24,8 +24,8 @@ class CommandLineTests(unittest.TestCase):
         os.system(
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed \
-            bygul/tests/data/reference.fasta \
+            --primers bygul/tests/data/ARTIC_V4-1.bed \
+            --reference bygul/tests/data/reference.fasta \
             --simulator wgsim --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
@@ -34,8 +34,8 @@ class CommandLineTests(unittest.TestCase):
         os.system(
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed \
-            bygul/tests/data/reference.fasta \
+            --primers bygul/tests/data/ARTIC_V4-1.bed \
+            --reference bygul/tests/data/reference.fasta \
             --maxmismatch 2 --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
@@ -44,8 +44,8 @@ class CommandLineTests(unittest.TestCase):
         os.system(
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed --readcnt 200 \
-            bygul/tests/data/reference.fasta --redo"
+            --primers bygul/tests/data/ARTIC_V4-1.bed --readcnt 200 \
+            --reference bygul/tests/data/reference.fasta --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
 
@@ -53,8 +53,8 @@ class CommandLineTests(unittest.TestCase):
         os.system(
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed --readlength 130 \
-            bygul/tests/data/reference.fasta --redo"
+            --primers bygul/tests/data/ARTIC_V4-1.bed --readlength 130 \
+            --reference bygul/tests/data/reference.fasta --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
 
@@ -63,8 +63,8 @@ class CommandLineTests(unittest.TestCase):
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta,\
             bygul/tests/data/KR-SEARCH-120354.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed \
-            bygul/tests/data/reference.fasta --redo"
+            --primers bygul/tests/data/ARTIC_V4-1.bed \
+            --reference bygul/tests/data/reference.fasta --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
 
@@ -73,8 +73,18 @@ class CommandLineTests(unittest.TestCase):
             "bygul simulate-proportions \
             bygul/tests/data/ATM-2FFMD73N3.fasta,\
             bygul/tests/data/KR-SEARCH-120354.fasta \
-            bygul/tests/data/ARTIC_V4-1.bed \
-            bygul/tests/data/reference.fasta \
+            --primers bygul/tests/data/ARTIC_V4-1.bed \
+            --reference bygul/tests/data/reference.fasta \
+            --proportions 0.8,0.2 --redo"
+        )
+        self.assertTrue(file_exists(".", "results/reads_1.fastq"))
+    
+    def test_simulation_with_metagenomics(self):
+        os.system(
+            "bygul simulate-proportions \
+            bygul/tests/data/ATM-2FFMD73N3.fasta,\
+            bygul/tests/data/KR-SEARCH-120354.fasta \
+            --simulation_mode metagenomics \
             --proportions 0.8,0.2 --redo"
         )
         self.assertTrue(file_exists(".", "results/reads_1.fastq"))
