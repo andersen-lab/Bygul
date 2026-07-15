5337d16ae32cd0025f89b766aac7d2b1f6b96dba
Maryam <mary.elf97@gmail.com>
2026-07-15 13:35:13 -0700

    update freyja proprotion tests to include more test cases

--- bygul/tests/data/test_proportions.py
+++ bygul/tests/data/test_proportions.py
@@ -1,55 +1,94 @@
-import pandas as pd
 import os
 import unittest
+
+import pandas as pd
 
 
 class ProportionTests(unittest.TestCase):
 
+    def _run_freyja_and_check(self, expected):
+        """Run Freyja demix and verify expected abundances."""
+        ref = "bygul/tests/data/reference.fasta"
+
+        os.system(
+            f"minimap2 -ax sr {ref} "
+            "results/reads_1.fastq results/reads_2.fastq | "
+            "samtools sort -o results/merged.bam"
+        )
+        os.system("samtools index results/merged.bam")
+
+        os.system(
+            "freyja variants results/merged.bam "
+            "--variants results/variants.tsv "
+            f"--depths results/depths.tsv --ref {ref}"
+        )
+        os.system("freyja update --outdir .")
+        os.system(
+            "freyja demix results/variants.tsv "
+            "results/depths.tsv "
+            "--output results/demix.tsv "
+            "--depthcutoff 20 "
+            "--lineageyml lineages.yml"
+        )
+
+        self.assertTrue(os.path.exists("results/demix.tsv"))
+
+        df = pd.read_csv("results/demix.tsv", sep="\t", index_col=0)
+        abundances = [
+            float(x) for x in df.loc["abundances"].values[0].split()
+        ]
+
+        for observed, exp in zip(abundances, expected):
+            self.assertAlmostEqual(observed, exp, delta=0.1)
+
     def test_prop_with_freyja(self):
-        # 1. Run the simulation
         os.system(
             "bygul simulate-proportions "
             "--genomes bygul/tests/data/BCN-SEARCH-105346.fasta,"
             "bygul/tests/data/CA-SEARCH-43254.fasta "
             "--primers bygul/tests/data/ARTIC_V4-1.bed "
             "--reference bygul/tests/data/reference.fasta "
-            "--proportions 0.8,0.2 --redo")
+            "--proportions 0.8,0.2 "
+            "--redo"
+        )
+
+        self.assertTrue(os.path.exists("results/reads_1.fastq"))
+        self._run_freyja_and_check([0.8, 0.2])
+
+    def test_simulation_with_csv(self):
+        os.system(
+            "bygul simulate-proportions "
+            "--primers bygul/tests/data/ARTIC_V4-1.bed "
+            "--csv bygul/tests/data/sample_proportions.csv "
+            "--multifasta bygul/tests/data/sample_genomes.fasta "
+            "--redo "
+            "--simulator mason "
+            "--reference bygul/tests/data/reference.fasta "
+            "--illumina-read-length 200"
+        )
+
         self.assertTrue(os.path.exists("results/reads_1.fastq"))
 
-        # 2. Run Freyja Pipeline (Align -> Variants -> Demix)
-        # Note: Replace 'minimap2' with your preferred aligner
-        ref = "bygul/tests/data/reference.fasta"
-        os.system(f"minimap2 -ax sr {ref} "
-                  "results/reads_1.fastq results/reads_2.fastq | "
-                  "samtools sort -o results/merged.bam")
-        os.system("samtools index results/merged.bam")
-        # Freyja commands
-        os.system("freyja variants results/merged.bam "
-                  "--variants results/variants.tsv "
-                  f"--depths results/depths.tsv --ref {ref}")
-        os.system("freyja update --outdir .")
-        os.system("freyja demix results/variants.tsv "
-                  "results/depths.tsv --output "
-                  "results/demix.tsv --depthcutoff 20 "
-                  "--lineageyml lineages.yml")
+        # Expected values should match sample_proportions.csv
+        self._run_freyja_and_check([0.8, 0.2])
 
-        # 3. Validate Proportions
-        self.assertTrue(os.path.exists("results/demix.tsv"))
+    def test_simulation_with_multiple_contig_samples(self):
+        os.system(
+            "bygul simulate-proportions "
+            "--primers bygul/tests/data/ARTIC_V4-1.bed "
+            "--csv bygul/tests/data/sample_proportions.csv "
+            "--multifasta bygul/tests/data/sample_genomes_multi_contig.fasta "
+            "--redo "
+            "--simulator mason "
+            "--reference bygul/tests/data/reference.fasta "
+            "--illumina-read-length 200"
+        )
 
-        # Read the Freyja output
-        df = pd.read_csv("results/demix.tsv", sep='\t', index_col=0)
+        self.assertTrue(os.path.exists("results/reads_1.fastq"))
 
-        # Access the string of numbers in the 'abundances' row
-        abundances_str = df.loc['abundances'].values[0]
-
-        abundances = [float(x) for x in abundances_str.split()]
-
-        top_abundance = abundances[0]
-        second_abundance = abundances[1]
-
-        self.assertAlmostEqual(top_abundance, 0.8, delta=0.1)
-        self.assertAlmostEqual(second_abundance, 0.2, delta=0.1)
+        # Expected values should match sample_proportions.csv
+        self._run_freyja_and_check([0.8, 0.2])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
