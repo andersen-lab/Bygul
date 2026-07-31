d7ef502ba5e5e78d26a087ecf9aa4c97004e4657
Maryam <mary.elf97@gmail.com>
2026-07-31 10:52:11 -0700

    bump version and remove redundant packages

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ description = ("Amplicon read simualtor")
 
 setup(
     name="bygul",
-    version="V4.0.0",
+    version="V4.0.1",
     packages=find_packages(include=['bygul']),
     author="Maryam Ahmadi Jeshvaghane",
     license='BSD 2-Clause',
@@ -33,7 +33,6 @@ setup(
         'bygul': ['data/*', ],
     },
     install_requires=[
-        "click", "pandas", "biopython",
-        "regex", "numpy", "tqdm"
+        "regex",
         ]
 )
